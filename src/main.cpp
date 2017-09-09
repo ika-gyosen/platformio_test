c5fed43c8aed223b619262957731d9ce67f603f2
酒井 隼 <sakaishun@ss420-2.local>
2017-09-09 12:24:17 +0900

    add openOCD setting

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+//#define __CORRECT_ISO_CPP_MATH_H_PROTO
 
+DigitalOut myled(LED1);
+//Serial pc(USBTX,USBRX);
+
+float x;
+float y = 0.5;
 int main() {
     while(1) {
         myled = 1; // LED is ON
         wait(0.2); // 200 ms
         myled = 0; // LED is OFF
-        wait(1.0); // 1 sec
+		wait(3.0); // 1 sec
+		x = abs(y);
+//		pc.printf("%f\n",x);
     }
 }
